5d95776abde0cc7a12d456640685fe800c67e9dd
Vegard Veiset <veiset@gmail.com>
2024-12-26 21:24:23 +0100

    Update landing page

--- src/pages/requests/Requests.tsx
+++ src/pages/requests/Requests.tsx
@@ -1,17 +1,23 @@
+import vendorIcon from "@/img/whetstone_inventory_icon.png";
 const Requests = () => {
   return (
     <div>
-      <h1>Path of Exile 2 Regex</h1>
-      <p>
-        Currently in development. New features will be added as the game is explored.
+      <h1 className="flex items-center justify-center lg:text-3xl pt-10">Path of Exile 2 Regex</h1>
+      <p className="flex items-center justify-center pt-10">
+        Currently in early development. New features will be added as the game is explored.
       </p>
-      <p>
-        <a
-          href="https://github.com/veiset/poe2.re/issues/new?assignees=veiset&labels=enhancement&projects=&template=feature_request.md&title=Feature%3A+%3CTitle%3E">Suggest
-          a feature</a>
+      <p className="flex items-center justify-center">
+        Please use Github to suggest features that would be nice to have or issues you find.
       </p>
-      <p>
-        Please use Github to suggest features that would be nice to have.
+
+      <h2 className="flex items-center justify-center lg:text-2xl pt-10">Current features:</h2>
+      <p className="flex items-center justify-center pt-5">
+        <div>
+          <a href="/vendor" className="flex">
+            <img src={vendorIcon} alt="vendor regex" width="32" height="32"/>
+            <span>Vendor Regex</span>
+          </a>
+        </div>
       </p>
 
     </div>
