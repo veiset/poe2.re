09500cad1a31c0880bc547f30efdf6d86528a3c5
Vegard Veiset <veiset@gmail.com>
2025-06-12 18:14:56 +0200

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {Menu} from "@/components/menu/Menu.tsx";
 import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar.tsx";
 import {useState} from "react";
 import {loadWebSettings, saveWebSettings} from "@/lib/localStorage.ts";
-import Requests from "@/pages/requests/Requests.tsx";
 import {Vendor} from "@/pages/vendor/Vendor.tsx";
 import {Waystone} from "@/pages/waystone/Waystone.tsx";
 import {Tablet} from "@/pages/tablet/Tablet.tsx";
